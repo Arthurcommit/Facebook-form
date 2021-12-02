a0f728fcb2b468240c01855f196f1e5b0d624501
Arthurcommit <66469924+Arthurcommit@users.noreply.github.com>
2021-12-02 16:38:14 +0100

    Inscription

--- inscription.php
+++ inscription.php
@@ -5,30 +5,33 @@
 $BDD = array();
 $BDD['host'] = "localhost";
 $BDD['user'] = "root";
-$BDD['pass'] = "";
-$BDD['db'] = "nom_de_la_base_de_donnees";
+$BDD['pass'] = "ROOT";
+$BDD['db'] = "test1";
 $mysqli = mysqli_connect($BDD['host'], $BDD['user'], $BDD['pass'], $BDD['db']);
 
 if(!$mysqli) {
     echo "Connexion non établie.";
     exit;
 }
-    //création automatique de la table membres:
+    //création automatique de la table "membres":
     echo mysqli_query($mysqli,"CREATE TABLE IF NOT EXISTS `".$BDD['db']."`.`membres` ( `id` INT NOT NULL AUTO_INCREMENT , `firstname` VARCHAR(25) NOT NULL , `lastname` VARCHAR(25) NOT NULL , `mail` VARCHAR(25) NOT NULL, `confirm_mail` VARCHAR(25) NOT NULL, `ddn` DATE NOT NULL, `mdp` CHAR(32) NOT NULL ,`sex` CHAR(32) NOT NULL, PRIMARY KEY (`id`)) ENGINE = MyISAM;")?"":"Erreur création table membres: ".mysqli_error($mysqli);
     
 //par défaut, on affiche le formulaire 
-$AfficherFormulaire=1;
+
 
 //traitement du formulaire:
 
-if(isset($_POST['bouton'])){ 
-    // $_post permet de récuperer dans la BDD les infos soumises dans le formulaire
-    // isset vérifie que la variable existe
-    //l'utilisateur à cliqué sur "S'inscrire", on demande donc si les champs sont défini avec "isset"
-    if(empty($_POST['firstname'])){//le champ firstname est vide, on arrête l'exécution du script et on affiche un message d'erreur
+
+    // $_post permet de récuperer dans la database les infos soumises dans le formulaire
+    // isset vérifie que la variable existe 
+    // l'utilisateur à cliqué sur "S'inscrire", on demande donc si les champs sont définis avec "isset"
+    if(empty($_POST)){ 
+    
+    }elseif(empty($_POST['firstname'])){ //le champ firstname est vide, on arrête l'exécution du script et on affiche un message d'erreur
         echo "Le champ Prénom est vide.";
         exit;
-    } elseif(empty($_POST['lastname'])){
+        
+    }elseif(empty($_POST['lastname'])){
         echo "Le champ Nom de famille est vide.";
         exit;
     }elseif(empty($_POST['mail'])){
@@ -87,113 +90,128 @@ if(isset($_POST['bouton'])){
                 echo "Une erreur s'est produite: ".mysqli_error($mysqli);
             } else {
                 echo "Vous êtes inscrit avec succès!";
+                exit;
                 //on affiche plus le formulaire
-                $AfficherFormulaire=0;
             }
         }
     }
-}
 
+    ?>
+    
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="style.css" />
+        <script src="test.js"></script>
+        <meta name="viewport" content="minimum-scale=1.0, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, width=device-width, shrink-to-fit=no"/>
+    </head>
+    <body>
+        <form action="connexion.php" method="post">
+            <!-- <div class="fb-header-base"></div> -->
+            <div class="fb-header">
+            
+                <div id="form1">
+                    <div class="flex">
+                        <div style="margin-top:8px;">Email or Phone</div>
+                        <input placeholder="Email" type="text" name="mail" value=""/>
+                    </div>
+                </div>
+                <div id="form2">
+                    <div class="flex">
+                        <div style="margin-top:8px;">Password</div>
+                        <input placeholder="Password" type="password" name="mdp"/>
+                        <div class="font-size">Informations de compte oubliées ? </div>
+                    </div>
+                </div>
+                <div id="form3">
+                    <div class="flex" style="height:21px; margin-top:8px">
+                        <!-- <div style="color:#3d5b99; margin-top:8px;">""</div> -->
+                        <input type="submit" class="submit1" name="connexion" value="Connexion"> 
+                    </div> 
+                </div>
+            </div>
+        </form>
     
 
-if($AfficherFormulaire==1){ 
-    ?>
 
-    <br />
-    <link type="text/css" rel="stylesheet" href="style.css" />
-
-<form action="connexion.php" method="post">
-    <div class="fb-header-base"></div>
-    <div class="fb-header">
-        <div id="form1" class="fb-header">
-            Email or Phone<br>
-            <input placeholder="Email" type="text" name="mail" value=""/><br />
-        </div>
-        <div id="form2" class="fb-header">
-            Password<br>
-            <input placeholder="Password" type="password" name="mdp"/><br /> 
-            Informations de compte oubliées ?
-        </div>
-    </div>
-    <input type="submit" class="submit1" name="connexion" value="Connexion">
-</form>
-
-
-<form method="post" action="inscription.php">
-    <div class="fb-body">
-	    <div id="intro1" class="fb-body"></div>
-	    <div id="intro2" class="fb-body">Inscription</div> 
-	    <div id="intro3" class="fb-body">C'est gratuit (et ça le restera toujours)</div>
-	    <div id="form3" class="fb-body">
-            <div> 
-                <div>
-                <input placeholder="Prénom" type="text" id="firstname" name="firstname" required>
-                <input placeholder="Nom de famille" type="text" id="lastname" name="lastname" required>
+        <form method="post" action="inscription.php">
+            <div class="fb-body">
+                <div id="form4" >
+                    <div> 
+                        <div class="center-element">
+                            <div id="intro2">Inscription</div> 
+                            <div id="intro3" >C'est gratuit (et ça le restera toujours)</div>
+                        </div>
+                        <div class="replace">
+                            <div>
+                                <input placeholder="Prénom" type="text" id="firstname" name="firstname" >
+                                <input placeholder="Nom de famille" type="text" id="lastname" name="lastname" >
+                            </div>
+                            <div id="nomprenom"> 
+                                <div id="missprenom"> </div> 
+                                <div id="missnom"> </div> 
+                            </div>
+                                <input placeholder="Email" id="mail" type="text" name="mail" ><div id="missmail"></div>
+                                <input placeholder="Confirmer votre email" type="text" id="confirm_mail" name="confirm_mail"><div id="missconfirm_mail"></div>
+                                <input placeholder="Nouveau mot de passe" type="password" id="mdp" name="mdp" ><div id="missmdp"></div>
+                                <p>Date de naissance</p> 
+                            <div>
+                                <select name="jour" id="day" > 
+                                    <option value="">Jour</option>
+                                    <?php 
+                                    for($i=0;$i<31;$i++) 
+                                    {
+                                    ?>
+                                    <option value="<?php echo $i+1;?>"> <?php echo $i+1; ?> </option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                                <select name="mois" id="month" >  
+                                    <option value="" >Mois</option>
+                                    <option value="1">Janvier</option>
+                                    <option value="2">Février</option>
+                                    <option value="3">Mars</option>
+                                    <option value="4">Avril</option>
+                                    <option value="5">Mai</option>
+                                    <option value="6">Juin</option>
+                                    <option value="7" >Juillet</option>
+                                    <option value="8">Aout</option>
+                                    <option value="9">Septembre</option>
+                                    <option value="10">Octobre</option>  
+                                    <option value="11">Novembre</option> 
+                                    <option value="12">Décembre</option>
+                                </select>
+                                <select name="annee" id="year" ><br>
+                                    <option value="">Année</option>
+                                    <?php
+                                    for($i=1899;$i<2021;$i++)
+                                    {
+                                        ?>
+                                        <option value="<?php echo $i+1;?>"> <?php echo $i+1; ?> </option>
+                                        <?php
+                                    }
+                                    ?> 
+                                </select>
+                            </div> 
+                            <div id="missdate"></div>
+                            <br />
+                            <div>        
+                                <input type="radio" id="r-b" name="sex" value="female" checked> Femme
+                                <input type="radio" id="r-b" name="sex" value="male" />Homme
+                            </div>
+                        </div>
+                    </div>
+                        <p id="intro4">En cliquant sur inscription, vous acceptez nos Conditions 
+                        et indiquez que vous avez lu notre Politique d'utilisation des données, y compris notre Utilisation des Cookies.
+                        Vous pourrez recevoir des notifications par texto de la part de Facebook et pouvez vous désabonner à tout moment.
+                        </p>
+                    <div class="green-btn">
+                        <input type="submit" id="btninscription" class="button2" name="bouton" value="Inscription" />
+                    </div>
                 </div>
-                <div id="nomprenom"> 
-                    <div id="missprenom"> </div> 
-                    <div id="missnom"> </div> 
-                </div>
-            </div>   
-            <input placeholder="Email" id="mail" type="text" name="mail" required><div id="missmail"></div>
-            <input placeholder="Confirmer votre email" type="text" id="confirm_mail" name="confirm_mail" required><div id="missconfirm_mail"></div>
-            <input placeholder="Nouveau mot de passe" type="password" id="mdp" name="mdp" required><div id="missmdp"></div>
-               <p>Date de naissance</p> 
-                <div>
-                    <select name="jour" id="day" required> 
-                        <option value="">Jour</option>
-                        <?php 
-                        for($i=0;$i<31;$i++) 
-                        {
-                        ?>
-                        <option value="<?php echo $i+1;?>"> <?php echo $i+1; ?> </option>
-                        <?php
-                        }
-                        ?>
-                    </select>
-                    <select name="mois" id="month" required>  
-                        <option value="" >Mois</option>
-                        <option value="1">Janvier</option>
-                        <option value="2">Février</option>
-                        <option value="3">Mars</option>
-                        <option value="4">Avril</option>
-                        <option value="5">Mai</option>
-                        <option value="6">Juin</option>
-                        <option value="7" >Juillet</option>
-                        <option value="8">Aout</option>
-                        <option value="9">Septembre</option>
-                        <option value="10">Octobre</option>  
-                        <option value="11">Novembre</option> 
-                        <option value="12">Décembre</option>
-                    </select>
-                    <select name="annee" id="year" required><br>
-                        <option value="">Année</option>
-                        <?php
-                        for($i=1899;$i<2021;$i++)
-                        {
-                            ?>
-                            <option value="<?php echo $i+1;?>"> <?php echo $i+1; ?> </option>
-                            <?php
-                        }
-                        ?> 
-                    </select>
-                </div> 
-                <div id="missdate"></div>
-                <br />
-                <div>        
-                    <input type="radio" id="r-b" name="sex" value="female" checked> Femme
-                    <input type="radio" id="r-b" name="sex" value="male" />Homme
-                </div>
-                <p id="intro4">En cliquant sur inscription, vous acceptez nos Conditions 
-                    et indiquez que vous avez lu notre Politique d'utilisation des données, y compris notre Utilisation des Cookies.
-                    Vous pourrez recevoir des notifications par texto de la part de Facebook et pouvez vous désabonner à tout moment.</p> </p> 
-            <input type="submit" id="btninscription" class="button2" name="bouton" value="Inscription" />
-        </div>
-    </div>  
-</form>
- 
+            </div>  
+        </form>
+    </body>
+<html>
 
- 
-<?php
-}
-?>  
+
